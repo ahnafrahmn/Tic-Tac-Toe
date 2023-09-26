80dea7ab700c6a5e4d276042d4b24f3e9dfcb658
Ahnaf <107554314+ahnafrahmn@users.noreply.github.com>
2023-09-26 10:49:33 +0600

    finished
    Tic-Tac-Toe Game
    
    Overview
    Welcome to the Tic-Tac-Toe game repository! This simple yet classic game is a perfect demonstration of your programming skills. Tic-Tac-Toe, also known as Noughts and Crosses, is a two-player game where the objective is to be the first to get three of your marks (either "X" or "O") in a row, column, or diagonal on a 3x3 grid.
    
    This GitHub repository contains a HTML, CSS and JavaScript implementation of the Tic-Tac-Toe game. It's an ideal project for beginners looking to learn the basics of JavaScript or for more experienced developers wanting to brush up on their skills. The code is clean, well-documented, and easy to understand.
    
    Features
    Two-Player Mode: Play against a friend locally, taking turns to make your moves.
    
    Interactive Board: A user-friendly interface that displays the current state of the game grid.
    
    Win Detection: The game automatically checks for a win or a draw and announces the winner or declares a draw.

---
+++ app.js
@@ -0,0 +1,134 @@
+
+//          Ahnaf's Codde
+//          IG : @ahnaf.rahmn
+//          G-mail : ahnaf.rahmn@gmail.com
+
+//==================================================================>>>
+let p1 = document.querySelector('.btn1'),
+p2 = document.querySelector('.btn2'),
+p3 = document.querySelector('.btn3'),
+p4 = document.querySelector('.btn4'),
+p5 = document.querySelector('.btn5'),
+p6 = document.querySelector('.btn6'),
+p7 = document.querySelector('.btn7'),
+p8 = document.querySelector('.btn8'),
+p9 = document.querySelector('.btn9'),
+
+winX = document.querySelector('.turnOfX'),
+winO = document.querySelector('.turnOfO'),
+turn_of_X = true,
+
+win_line_1 = document.querySelector('.line1'),
+win_line_2 = document.querySelector('.line2'),
+win_line_3 = document.querySelector('.line3'),
+win_line_4 = document.querySelector('.line4'),
+win_line_5 = document.querySelector('.line5'),
+win_line_6 = document.querySelector('.line6'),
+win_line_7 = document.querySelector('.line7'),
+win_line_8 = document.querySelector('.line8'),
+
+arrFor_X = [],
+arrFor_O = []
+
+winner = 'X', draw=true;
+
+
+const arrLine = [ win_line_1, win_line_2, win_line_3, win_line_4, win_line_5, win_line_6, win_line_7, win_line_8 ];
+const CheckArr = [ false, false, false, false, false, false, false, false, false ];
+const btnArr = [ p1, p2, p3, p4, p5, p6, p7, p8, p9 ];
+
+//========================================================>>>>>>>>
+
+
+for(let i=0; i<btnArr.length; i++){
+    const tmpbtn = btnArr[i];
+    tmpbtn.addEventListener('click', function() {
+        tmpbtn.classList.add('buttonClicked');
+        if(!CheckArr[i]){
+            if(turn_of_X){
+                tmpbtn.innerHTML = 'X';
+                arrFor_X.push(i)
+                CheckArr[i]=true;
+                turn_of_X = false;
+                let tmpVar = checkWin(arrFor_X);
+                winner = 'X';
+                winLine(tmpVar);
+            } else{
+                tmpbtn.innerHTML = 'O';
+                arrFor_O.push(i)
+                CheckArr[i]=true;
+                turn_of_X = true;
+                let tmpVar = checkWin(arrFor_O);
+                winner = 'O';
+                winLine(tmpVar);
+            }
+        }
+        checkTurn();
+    })
+}
+
+function checkTurn(){
+    if(turn_of_X){
+        winX.classList.add('bright')
+        winO.classList.remove('bright')
+    } else{
+        winX.classList.remove('bright')
+        winO.classList.add('bright')
+    }
+    if(arrFor_O.length + arrFor_X.length == 9 && draw==true){
+        sleep(400).then(() => {
+            alert("It's a DRAW!!");
+            location.reload();
+        });
+        
+    }
+}
+checkTurn();
+
+// this function checks if its a win
+function checkWin(arr){
+    arr.sort();
+    if(arr.includes(0) && arr.includes(3) && arr.includes(6)){
+        return 'win1';
+    } else if(arr.includes(1) && arr.includes(4) && arr.includes(7)){
+        return 'win2';
+    } else if(arr.includes(2) && arr.includes(5) && arr.includes(8)){
+        return 'win3';
+    } else if(arr.includes(0) && arr.includes(1) && arr.includes(2)){
+        return 'win4';
+    } else if(arr.includes(3) && arr.includes(4) && arr.includes(5)){
+        return 'win5';
+    } else if(arr.includes(6) && arr.includes(7) && arr.includes(8)){
+        return 'win6';
+    } else if(arr.includes(0) && arr.includes(4) && arr.includes(8)){
+        return 'win7';
+    } else if(arr.includes(2) && arr.includes(4) && arr.includes(6)){
+        return 'win8';
+    } else{
+        return 'x'
+    }
+}
+
+// this function is for the winning line after someone wins
+function winLine(tmpVar){
+    if(tmpVar != 'x'){
+        let tmpNum = Number(tmpVar[tmpVar.length-1]) - 1;
+        draw = false;
+        if(tmpNum<3){
+            arrLine[tmpNum].classList.add('verticalWinLine');
+        } else if(tmpNum<6){
+            arrLine[tmpNum].classList.add('horizontalWinLine');
+        } else{
+            arrLine[tmpNum].classList.add('crossWinLine');
+        }
+        sleep(1000).then(() => {
+            alert(`Team : "${winner}" won the game!!`);
+            location.reload();
+        });
+    }
+}
+
+// for timeout before refrashing
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
